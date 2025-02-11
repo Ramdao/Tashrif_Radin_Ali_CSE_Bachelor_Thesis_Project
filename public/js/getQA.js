68a4351ba7455362f4f25b44455083aeffad8f17
Tashrif Radin Ali <radin00@hotmail.com>
2025-02-11 12:46:43 -0500

    fix qaa

--- public/js/getQA.js
+++ public/js/getQA.js
@@ -1,30 +1,25 @@
+window.onload = function() {
+    let q = [];
+    let a = [];
 
-q=[]
-a=[]
-
-const request = new XMLHttpRequest();
-request.open("GET", "https://tashrif-radin-ali-cse-bachelor-thesis.onrender.com/user");
+    const request = new XMLHttpRequest();
+    request.open("GET", "https://tashrif-radin-ali-cse-bachelor-thesis.onrender.com/user");
     
     request.onload = function() {
-        
         if (request.status == 200) {
-            
             const response = JSON.parse(request.responseText);
-            var user = response[0];
-            let searchuser = JSON.parse(localStorage.getItem("user"))
-            for (let i =0; i<response.length;i++){
-                if(response[i].email==searchuser){
-                    user=response[i]
+            let user = response[0];
+            let searchuser = JSON.parse(localStorage.getItem("user"));
+            for (let i = 0; i < response.length; i++) {
+                if (response[i].email == searchuser) {
+                    user = response[i];
                 }
             }
-            
             localStorage.setItem("question", JSON.stringify(user.questions));
             localStorage.setItem("answer", JSON.stringify(user.answers));
-           
         } else {
             console.error("Request failed with status:", request.status);
         }
-       
     };
     
     request.onerror = function() {
@@ -33,131 +28,110 @@ request.open("GET", "https://tashrif-radin-ali-cse-bachelor-thesis.onrender.com/
 
     request.send();
 
-question=JSON.parse(localStorage.getItem("question"))
-answer=JSON.parse(localStorage.getItem("answer"))
+    let question = JSON.parse(localStorage.getItem("question"));
+    let answer = JSON.parse(localStorage.getItem("answer"));
 
+    question = question.replace(/\\/g, '');
+    answer = answer.replace(/\\/g, '');
 
-question = question.replace(/\\/g, '');
-answer = answer.replace(/\\/g, '');
+    question = JSON.parse(question);
+    answer = JSON.parse(answer);
 
-question=JSON.parse(question)
-answer=JSON.parse(answer)
+    document.getElementById("btn2").disabled = true;
 
-document.getElementById("btn2").disabled =true;
-
-if (question.length >=2){
-    document.getElementById("btn2").disabled =false;
-
-}
-
-
-
-
-function next(){
-    if(document.getElementById("Question").value!="" && document.getElementById("Answer").value!=""){
-        q.push(document.getElementById("Question").value)
-        a.push (document.getElementById("Answer").value)
-       
+    if (question.length >= 2) {
+        document.getElementById("btn2").disabled = false;
     }
-    
-    document.getElementById("Question").value=""
-    document.getElementById("Answer").value=""
-   
-}
 
-function submit(){
-    
-    var data = {
-        "email":JSON.parse(localStorage.getItem("user")),
-        "questions":JSON.stringify(q) ,
-        "answers":JSON.stringify(a),
-    } 
-    const request = new XMLHttpRequest();
-    request.open("POST", "https://tashrif-radin-ali-cse-bachelor-thesis.onrender.com/user/add-question");
-
-    request.setRequestHeader("Access-Control-Allow-Credentials", "true");
-    request.setRequestHeader("Content-Type", "application/json");
-
-
-    request.send(JSON.stringify(data));
-
-
-request.open("GET", "https://tashrif-radin-ali-cse-bachelor-thesis.onrender.com/user");
-    
-    request.onload = function() {
-        
-        if (request.status == 200) {
-            
-            const response = JSON.parse(request.responseText);
-            var user = response[0];
-            let searchuser = JSON.parse(localStorage.getItem("user"))
-            for (let i =0; i<response.length;i++){
-                if(response[i].email==searchuser){
-                    user=response[i]
-                }
-            }
-            
-            localStorage.setItem("question", JSON.stringify(user.questions));
-            localStorage.setItem("answer", JSON.stringify(user.answers));
-           
-        } else {
-            console.error("Request failed with status:", request.status);
+    function next() {
+        if (document.getElementById("Question").value != "" && document.getElementById("Answer").value != "") {
+            q.push(document.getElementById("Question").value);
+            a.push(document.getElementById("Answer").value);
         }
-       
-    };
-    
-    request.onerror = function() {
-        console.error("Network error occurred");
-    };
 
-    request.send();
+        document.getElementById("Question").value = "";
+        document.getElementById("Answer").value = "";
+    }
 
-question=JSON.parse(localStorage.getItem("question"))
-answer=JSON.parse(localStorage.getItem("answer"))
+    function submit() {
+        let data = {
+            "email": JSON.parse(localStorage.getItem("user")),
+            "questions": JSON.stringify(q),
+            "answers": JSON.stringify(a),
+        };
 
+        const request = new XMLHttpRequest();
+        request.open("POST", "https://tashrif-radin-ali-cse-bachelor-thesis.onrender.com/user/add-question");
 
-question = question.replace(/\\/g, '');
-answer = answer.replace(/\\/g, '');
+        request.setRequestHeader("Access-Control-Allow-Credentials", "true");
+        request.setRequestHeader("Content-Type", "application/json");
 
-question=JSON.parse(question)
-answer=JSON.parse(answer)
+        request.send(JSON.stringify(data));
 
-if (question.length >=2){
-    document.getElementById("btn2").disabled =false;
+        request.open("GET", "https://tashrif-radin-ali-cse-bachelor-thesis.onrender.com/user");
 
-}
+        request.onload = function() {
+            if (request.status == 200) {
+                const response = JSON.parse(request.responseText);
+                let user = response[0];
+                let searchuser = JSON.parse(localStorage.getItem("user"));
+                for (let i = 0; i < response.length; i++) {
+                    if (response[i].email == searchuser) {
+                        user = response[i];
+                    }
+                }
 
-    
-    
-}
+                localStorage.setItem("question", JSON.stringify(user.questions));
+                localStorage.setItem("answer", JSON.stringify(user.answers));
+            } else {
+                console.error("Request failed with status:", request.status);
+            }
+        };
 
-function clearQ(){
-    count=0
-    var data = {
-        "email":JSON.parse(localStorage.getItem("user")),
-    } 
-    const request = new XMLHttpRequest();
-    request.open("POST", "https://tashrif-radin-ali-cse-bachelor-thesis.onrender.com/user/remove-question");
+        request.onerror = function() {
+            console.error("Network error occurred");
+        };
 
-    request.setRequestHeader("Access-Control-Allow-Credentials", "true");
-    request.setRequestHeader("Content-Type", "application/json");
+        request.send();
 
+        question = JSON.parse(localStorage.getItem("question"));
+        answer = JSON.parse(localStorage.getItem("answer"));
 
-    request.send(JSON.stringify(data));
-    
-    
-   
-}
+        question = question.replace(/\\/g, '');
+        answer = answer.replace(/\\/g, '');
 
-function howto(){
-    window.location="Howto.html";
-}
+        question = JSON.parse(question);
+        answer = JSON.parse(answer);
 
+        if (question.length >= 2) {
+            document.getElementById("btn2").disabled = false;
+        }
+    }
 
-function game2(){
-    window.location="Playarea.html";
-}
+    function clearQ() {
+        count = 0;
+        let data = {
+            "email": JSON.parse(localStorage.getItem("user")),
+        };
 
-function back(){
-    window.location="index.html";
-}
+        const request = new XMLHttpRequest();
+        request.open("POST", "https://tashrif-radin-ali-cse-bachelor-thesis.onrender.com/user/remove-question");
+
+        request.setRequestHeader("Access-Control-Allow-Credentials", "true");
+        request.setRequestHeader("Content-Type", "application/json");
+
+        request.send(JSON.stringify(data));
+    }
+
+    function howto() {
+        window.location = "Howto.html";
+    }
+
+    function game2() {
+        window.location = "Playarea.html";
+    }
+
+    function back() {
+        window.location = "index.html";
+    }
+};
